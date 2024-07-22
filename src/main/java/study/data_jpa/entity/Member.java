f2465197f16a3493561e151f37b0118ca392ed1b
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-07-22 21:22:36 +0900

    2024.07.22 스프링데이터JPA 시작

---
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -0,0 +1,40 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String userName;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String userName) {
+        this.userName = userName;
+    }
+
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    //연관관계 메서드
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+}
